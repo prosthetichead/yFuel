f047dd0a17a0fc7ab1c98613ddd1218fc050b9b5
Ashley <prosthetichead@gmail.com>
2015-07-12 23:22:36 +0930

    initial sync

--- www/js/app.js
+++ www/js/app.js
@@ -2,6 +2,7 @@
 // yFuel
 
 var app = angular.module('yFuel', ['ionic'])
+
 app.config(function($stateProvider, $urlRouterProvider) {
   $urlRouterProvider.otherwise('/fuel')
 
@@ -21,9 +22,11 @@ app.config(function($stateProvider, $urlRouterProvider) {
   	templateUrl: 'fuelIndex.html',
   	controller: 'fuelIndexCtrl'
   })
+  //Fuel Record Details view
   $stateProvider.state('fuel.detail', {
   	url: '/:fuelRecordIndex',
-  	templateUrl: 'fuelDetail.html'
+  	templateUrl: 'fuelDetail.html',
+  	controller: 'fuelDetailCtrl'	
   })
 
   //Help Tab
@@ -35,13 +38,109 @@ app.config(function($stateProvider, $urlRouterProvider) {
   //Settings Tab
   $stateProvider.state('settings', {
   	url: '/settings',
-  	views: { settings: { templateUrl: 'settings.html' }}
+  	views: { 
+  		settings: { 
+  			templateUrl: 'settings.html',
+  			controller: 'settingsCtrl'
+  		}
+  	}
   })
 })
 
-app.controller('fuelIndexCtrl', function($scope){
-	$scope.fuelRecords = [
-		{title: "07-07-15 10:36pm"},
-		{title: "07-07-15 10:00pm"}
-	]
+//////
+///Factorys
+///
+//Factory for Fuel Records
+//This is where we will eventualy read and write to a webservice or database?
+app.factory('fuelRecordsService', function() {
+  return {
+    all: function(){
+    	var fuelRecordsString = window.localStorage['fuelRecords'];
+    	if(fuelRecordsString) {
+    		return angular.fromJson(fuelRecordsString);
+    	}
+    	return [];
+    },
+    save: function(fuelRecords) {
+    	window.localStorage['fuelRecords'] = angular.toJson(fuelRecords);
+    },
+    getFuelRecord: function(index) {
+    	return getFromLocalStorage('fuelRecords')[index];
+    }
+  }
 })
+//Factory for Settings
+//For all the settings and units of mesure
+app.factory('settingsService', function(){
+	var settings = {
+		firstName: "First Name",
+		lastName: "Last Name",
+		email: "Email",
+	 	uomDistance: "Km",
+		uomVolume: "L",
+		car_rego: " "
+	}
+	return {
+		settings: settings
+	}
+})
+
+///////
+///Controllers
+///
+//Fuel Index Page Controler
+//Gets the fuel records from the factory fuelRecordsService
+app.controller('fuelIndexCtrl', function($scope, $ionicModal, fuelRecordsService){
+	$scope.fuelRecords = fuelRecordsService.all();
+
+	$ionicModal.fromTemplateUrl('fuelRecordModal.html', function(modal) {
+		$scope.fuelRecordModal = modal;
+	}, {
+    	scope: $scope,
+   	 	animation: 'slide-in-up'
+  	});
+
+  	$scope.addFuelRecord = function(fuelRecord) {
+  		$scope.fuelRecords.push({
+  			date_time: fuelRecord.date_time,
+  			odometer_reading: fuelRecord.odometer_reading,
+  			personal: fuelRecord.personal,
+  			trip_purpose: fuelRecord.trip_purpose,
+  			reason: fuelRecord.reason
+  		});
+
+  		$scope.fuelRecordModal.hide();
+  		fuelRecordsService.save($scope.fuelRecords);
+  	};
+
+	$scope.showFuelRecordModal = function() {
+    	$scope.fuelRecordModal.show();
+  	};
+  	$scope.hideFuelRecordModal = function() {
+  		$scope.fuelRecordModal.hide();
+  	};
+})
+//Fuel Detail Page Controler
+//Takes in the state paramiters from the state and use them to get the fuel record.
+app.controller('fuelDetailCtrl', function($scope, $state, $stateParams, fuelRecordsService){
+	$scope.fuelRecord = fuelRecordsService.getFuelRecord($stateParams.fuelRecordIndex)
+})
+//Settings Conroler
+app.controller('settingsCtrl', function($scope, settingsService){
+	$scope.settings = settingsService.settings;
+
+	$scope.updateSettings = function(settings) {
+		$scope.settings = settings;
+		settingsService.settings = settings;
+	}
+})
+
+
+////////
+///Custom Functions
+///
+//Gets objects back out of local storage
+function getFromLocalStorage(key){
+    	var localStorageString = window.localStorage['fuelRecords']; 
+    	return angular.fromJson(localStorageString);
+}
